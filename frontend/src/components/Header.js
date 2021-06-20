f86806426821051275562d06b821b347552524b8
Michael Mogannam <mogannammichael@gmail.com>
2021-06-19 20:10:42 -0700

    admin userList

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -34,15 +34,30 @@ const Header = () => {
                             <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
                         </LinkContainer>
                         {userInfo ? (
-                            <NavDropdown title={userInfo.name} id='username'>
+                            <NavDropdown title={userInfo.first_name} id='username'>
                                 <LinkContainer to='/profile'>
                                     <NavDropdown.Item>Profile</NavDropdown.Item>
                                 </LinkContainer>
                                 <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                             </NavDropdown>
-                        ) : <LinkContainer to="/login">
+                        ) : (
+                            <LinkContainer to="/login">
                         <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
-                    </LinkContainer>}
+                    </LinkContainer>
+                    )}
+                    {userInfo && userInfo.isAdmin && (
+                        <NavDropdown title='Admin' id='adminMenu'>
+                        <LinkContainer to='/admin/userList'>
+                            <NavDropdown.Item>Users</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/admin/productList'>
+                            <NavDropdown.Item>Products</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to='/admin/orderList'>
+                            <NavDropdown.Item>Orders</NavDropdown.Item>
+                        </LinkContainer>
+                    </NavDropdown>
+                    )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
